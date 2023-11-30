c3b3c09bdeb996f3dffe5ff0cd314e5c82cebaab
Minh Triet <triettran332003@gmail.com>
2023-11-30 18:32:08 +0700

    Update comments

--- nachos/nachos-3.4/code/test/teststring.c
+++ nachos/nachos-3.4/code/test/teststring.c
@@ -5,11 +5,16 @@
 
 int main()
 {
+	//Tạo 1 mảng gồm 255 ký tự ( có thể nhiều hơn tùy vào sở thích)
+	//Đặt biến len bằng MaxSize có thể có
+
 	char buffer[255];
 	int len = MaxSize;
 	
+	//Gọi syscall ReadString với 2 tham số buffer, và MaxSize để tiến hành đọc dữ liệu từ màn hình Console
 	ReadString(buffer, MaxSize);
 	
+	// Tiến hành in ra màn hính với syscall PrintString
 	PrintString(buffer);
 
 	Halt();
