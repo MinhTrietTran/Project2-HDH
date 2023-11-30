48fe1322ce0341561b2bf4a6741289f1f38aedf1
TLHcmus <118470730+TLHcmus@users.noreply.github.com>
2023-11-30 19:15:41 +0700

    Update comment testhelp.c

--- nachos/nachos-3.4/code/test/testhelp.c
+++ nachos/nachos-3.4/code/test/testhelp.c
@@ -3,6 +3,7 @@
 
 int main()
 {   
+    // Goi system call PrintString de in gioi thieu co ban ve nhom
     PrintString("\n==========HE DIEU HANH- 2023==========\n");
     PrintString("THONG TIN NHOM\n");
     PrintString("21120515 - TRAN PHUOC NHAN\n");
@@ -11,7 +12,7 @@ int main()
     PrintString("21120585 - LE ANH TU\n");
     PrintString("21120587 - NGUYEN MINH TUAN\n");
     PrintString("\n======================================\n");
-
+    // Goi system call PrintString de in mo ta van tat ve chuong trinh ascii
     PrintString("\n==========IN MA ASCII==========\n");
     PrintString("Bang ma ASCII se co ky tu in duoc va khong\n");
     PrintString("Trong bang ma co 1 ky tu dang tien chuong khong duoc hien thi\n");
@@ -19,8 +20,8 @@ int main()
     PrintString("Dat terminal tai ../nachos/nachos-3.4/code\n");
     PrintString("Dung lenh: ./userprog/nachos -rs 1023 -x ./test/testascii\n");
     PrintString("\n======================================\n");
-
-      PrintString("\n==========SORT==========\n");
+    // Goi system call PrintString de in mo ta van tat ve chuong trinh sort
+    PrintString("\n==========SORT==========\n");
     PrintString("Thuat toan Bubble Sort\n");
     PrintString("Nhap vao so luong phan tu n\n");
     PrintString("Nhap cac phan tu trong mang can sap xep\n");
